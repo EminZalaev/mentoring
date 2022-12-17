23fa72bcdf153194a4929592622e56dfdfbb1b25
Залаев Мухаммад-Эмин Резванович <zalaev.m@wb.ru>
2022-12-18 00:52:54 +0300

    add cors

--- internal/handlers.go
+++ internal/handlers.go
@@ -2,19 +2,22 @@ package internal
 
 import (
 	"encoding/json"
-
 	"github.com/gofiber/fiber/v2"
+	"net/http"
 )
 
 func getCurrency(s Store) fiber.Handler {
 	return func(ctx *fiber.Ctx) error {
+		writeCors(ctx)
 		currency, err := s.GetCurrency()
 		if err != nil {
+			writeError(ctx, "internal error", http.StatusInternalServerError)
 			return err
 		}
 
 		body, err := json.Marshal(currency)
 		if err != nil {
+			writeError(ctx, "internal error", http.StatusInternalServerError)
 			return err
 		}
 
@@ -25,13 +28,16 @@ func getCurrency(s Store) fiber.Handler {
 
 func postCurrency(s Store) fiber.Handler {
 	return func(ctx *fiber.Ctx) error {
+		writeCors(ctx)
 		currency := &currencyRequest{}
 		if err := json.Unmarshal(ctx.Request().Body(), &currency); err != nil {
+			writeError(ctx, "wrong json", http.StatusBadRequest)
 			return err
 		}
 
 		err := s.PostCurrency(currency)
 		if err != nil {
+			writeError(ctx, "internal error", http.StatusInternalServerError)
 			return err
 		}
 
@@ -41,16 +47,30 @@ func postCurrency(s Store) fiber.Handler {
 
 func putCurrency(s Store) fiber.Handler {
 	return func(ctx *fiber.Ctx) error {
+		writeCors(ctx)
 		currency := &currencyRequest{}
 		if err := json.Unmarshal(ctx.Request().Body(), &currency); err != nil {
+			writeError(ctx, "wrong json", http.StatusBadRequest)
 			return err
 		}
 
 		err := s.PutCurrency(currency)
 		if err != nil {
+			writeError(ctx, "internal error", http.StatusInternalServerError)
 			return err
 		}
 
 		return nil
 	}
+}
+
+func writeCors(ctx *fiber.Ctx) {
+	ctx.Response().Header.Set("Access-Control-Allow-Credentials", "*")
+	ctx.Response().Header.Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	ctx.Response().Header.SetBytesV("Access-Control-Allow-Origin", []byte("Accept, Content-Type, Content-Length, Accept-Encoding"))
+}
+
+func writeError(ctx *fiber.Ctx, msg string, statusCode int) {
+	ctx.Request().SetBodyString(msg)
+	ctx.Status(statusCode)
 }
