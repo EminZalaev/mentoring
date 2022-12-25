c3e032e8f81fbb202f23a8e2540e3dac2446df63
Залаев Мухаммад-Эмин Резванович <zalaev.m@wb.ru>
2022-12-25 14:25:13 +0300

    delete all

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM golang:1.18
-RUN mkdir /app
-ADD . /app/
-WORKDIR /app
-EXPOSE 8080
-
-CMD go run main.go
--- migrations/00001_.up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS currency
-(
-   currencyfrom text,
-   currencyto text,
-   well float default 0.0,
-   updated_at timestamptz
-)
